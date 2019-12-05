48d6e3a7da4a649ebb76213c58ac8294aa4e91b0
skyreaper925 <volodinmaxim1995@gmail.com>
2019-12-05 18:30:45 +0300

    A bit of copypaste

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,17 @@
 from django import forms
 
 from .models import Post
+from .models import User
+
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'text')
+        fields = ('title', 'text',)
+
+class UserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'password',)
--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,26 @@
-# Create your models here.
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-class Post (models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length = 200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank = True, null = True)
 
-def publish(self):
-    self.published_date = timezone.now()
-    self.save()
+class Post(models.Model):
+    # создаём поля для объекта модели поста, который будет находится в бд
+    #author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # ссылка на другую модель
+    title = models.CharField(max_length=200) # ограниченый размер переменной
+    text = models.TextField() # текст для поста
+    created_date = models.DateTimeField(default=timezone.now) # дата
+    published_date = models.DateTimeField(blank=True, null=True) # дата
 
-def __str__(self):
-    return self.title
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class User(models.Model):
+    username = models.CharField(max_length=100) # ограниченый размер переменной, потому что user не должен быть большим
+    password = models.TextField() # пароль может быть любой длины
+
+    def __str__(self):
+        return self.username
